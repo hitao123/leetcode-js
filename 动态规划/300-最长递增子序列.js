49f2cbde319d9a4ce7714d33aac4c32748be84be
hitao123 <hitao1234@gmail.com>
2022-07-25 10:09:36 +0800

    feat: update

--- 动态规划/300-最长递增子序列.js
+++ 动态规划/300-最长递增子序列.js
@@ -20,4 +20,36 @@ var lengthOfLIS = function (nums) {
     }
 
     return res
+
+    // 下面是二分法 复杂度更低，但是很难想到这种解法，还是上面的暴力破解更容易理解
+    // let top = new Array(nums.length);
+    // // 牌堆数初始化为 0
+    // let piles = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     // 要处理的扑克牌
+    //     let poker = nums[i];
+
+    //     /***** 搜索左侧边界的二分查找 *****/
+    //     let left = 0, right = piles;
+    //     while (left < right) {
+    //         let mid = Math.floor((left + right) / 2);
+    //         if (top[mid] > poker) {
+    //             right = mid;
+    //         } else if (top[mid] < poker) {
+    //             left = mid + 1;
+    //         } else {
+    //             right = mid;
+    //         }
+    //     }
+    //     // 没找到合适的牌堆，新建一堆
+    //     if (left == piles) piles++;
+    //     // 把这张牌放到牌堆顶
+    //     top[left] = poker;
+    // }
+    // // 牌堆数就是 LIS 长度
+    // return piles;
 };
+
+const arr = [2, 3, 6, 5, 1, 10]
+
+lengthOfLIS(arr)
--- 贪婪/134-加油站.js
+++ 贪婪/134-加油站.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 var canCompleteCircuit = function (gas, cost) {
+    let sum = 0;
+    let minSum = 0;
+    let len = gas.length
+    let start = 0
 
+    for (let i = 0; i < len; i++) {
+        sum += gas[i] - cost[i]
+        if (sum < minSum) {
+            start = i + 1
+            minSum = sum
+        }
+    }
+
+    if (sum < 0) {
+        return -1
+    }
+
+    return start == len ? 0 : start
 };
 
 // 输入: gas = [2, 3, 4], cost = [3, 4, 3]
